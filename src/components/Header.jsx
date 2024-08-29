4fcf6ad18c1d6918a5c0f3ad28381c8ab7a8eb10
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2024-08-29 20:42:29 +0300

    feat: added new links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
         <div className='header'>
             <img src="logo.png" alt="logo" className="logo" />
             <div className="links">
-                <a href="#"><i className="bi bi-microsoft"></i> Outlook</a>
-                <a href="#"><i className="bi bi-google"></i> Gmail</a>
+                <a href="outlook.com"><i className="bi bi-microsoft"></i> Outlook</a>
+                <a href="https://mail.google.com/mail/u/0/#inbox"><i className="bi bi-google"></i> Gmail</a>
             </div>
         </div>
     )
