acd2eb618e5d8b0480968e2440343d08977adb2a
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2024-08-29 20:44:41 +0300

    refactor: edited link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className='header'>
             <img src="logo.png" alt="logo" className="logo" />
             <div className="links">
-                <a href="outlook.com"><i className="bi bi-microsoft"></i> Outlook</a>
+                <a href="https://outlook.com"><i className="bi bi-microsoft"></i> Outlook</a>
                 <a href="https://mail.google.com/mail/u/0/#inbox"><i className="bi bi-google"></i> Gmail</a>
             </div>
         </div>
