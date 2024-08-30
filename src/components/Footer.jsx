5664fdc2a6e441483d7e73e4854eedd784847b15
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2024-08-30 12:13:02 +0300

    feat: added current time to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomizePopup from './CustomizePopup';
 
 function Footer() {
     const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [time, setTime] = useState('');
+
+    useEffect(() => {
+        // Saat güncelleme işlevi
+        const updateClock = () => {
+            const currentTime = new Date();
+            const hours = currentTime.getHours().toString().padStart(2, '0'); // Saati 2 basamaklı yapar
+            const minutes = currentTime.getMinutes().toString().padStart(2, '0'); // Dakikayı 2 basamaklı yapar
+            setTime(`${hours}:${minutes}`);
+        };
+
+        updateClock(); // İlk başta saat ayarlanır
+
+        const intervalId = setInterval(updateClock, 1000); // Her saniye güncellenir
+
+        return () => clearInterval(intervalId); // Bileşen kaldırıldığında interval temizlenir
+    }, []);
 
     const handleCustomizeClick = () => {
         setIsPopupOpen(true);
@@ -15,14 +32,19 @@ function Footer() {
     const handleSaveSettings = (settings) => {
         localStorage.setItem('bgSettings', JSON.stringify(settings));
         window.location.reload(); // Ayarları uygulamak için sayfayı yeniden yükleyin
-      };
+    };
 
     return (
         <div className='footer-container'>
-            <a href="#" onClick={handleCustomizeClick}><i className="bi bi-gear-fill"></i> Özelleştir</a>
-            {isPopupOpen && (
-                <CustomizePopup onSave={handleSaveSettings} onClose={handleClosePopup} />
-            )}
+            <div className="clock">
+                {time} {/* Saat burada gösterilecek */}
+            </div>
+            <div className="customize-container">
+                <a href="#" onClick={handleCustomizeClick}><i className="bi bi-gear-fill"></i> Özelleştir</a>
+                {isPopupOpen && (
+                    <CustomizePopup onSave={handleSaveSettings} onClose={handleClosePopup} />
+                )}
+            </div>
         </div>
     );
 }
